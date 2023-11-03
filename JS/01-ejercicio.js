f2afce34e5554bf9e4860e52338bd387a3e86882
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-11-03 12:09:22 -0300

    Agrego ejercicio numero 1

---
+++ JS/01-ejercicio.js
@@ -0,0 +1,11 @@
+/*Escribir un programa que solicite la edad y si es mayor de 18 años 
+mostrar un mensaje que ya puede conducir, si la edad ingresada no es 
+un número válido indicarlo en un mensaje*/
+
+const edad = parseInt(prompt('Ingrese su edad'));
+
+if (edad >= 18){
+    document.write('Ya puede conducir');
+    }else{
+        document.write('Aún no puede conducir');
+    }
