69d4a40dd9fba20cf733b10fb29199ea28a8b447
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-11-05 20:39:09 -0300

    Agrego switch del ejercicio numero 10 y termino el mismo

--- JS/05-ejercicio.js
+++ JS/05-ejercicio.js
@@ -11,11 +11,84 @@ Si lo introducido no es un número deberá indicarse con un alert y volver a pre
 Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 */
 
-const dni = parseInt(prompt('Ingrese su numero de DNI'))
+const dni = parseInt(prompt("Ingrese su numero de DNI"));
+
 if (isNaN(dni)) {
-    alert('Por favor, introduce un número válido de DNI.');
+  alert("Por favor, introduce un número válido de DNI.");
 } else if (dni >= 0 && dni <= 99999999) {
-    alert('Número de DNI válido.');
+  const resto = dni % 23;
+  switch (resto) {
+    case 1:
+      document.write("La letra correspondiente es T");
+      break;
+    case 2:
+      document.write("La letra correspondiente es R");
+      break;
+    case 3:
+      document.write("La letra correspondiente es W");
+      break;
+    case 4:
+      document.write("La letra correspondiente es A");
+      break;
+    case 5:
+      document.write("La letra correspondiente es G");
+      break;
+    case 6:
+      document.write("La letra correspondiente es M");
+      break;
+    case 7:
+      document.write("La letra correspondiente es Y");
+      break;
+    case 8:
+      document.write("La letra correspondiente es F");
+      break;
+    case 9:
+      document.write("La letra correspondiente es P");
+      break;
+    case 10:
+      document.write("La letra correspondiente es D");
+      break;
+    case 11:
+      document.write("La letra correspondiente es X");
+      break;
+    case 12:
+      document.write("La letra correspondiente es B");
+      break;
+    case 13:
+      document.write("La letra correspondiente es N");
+      break;
+    case 14:
+      document.write("La letra correspondiente es J");
+      break;
+    case 15:
+      document.write("La letra correspondiente es Z");
+      break;
+    case 16:
+      document.write("La letra correspondiente es S");
+      break;
+    case 17:
+      document.write("La letra correspondiente es Q");
+      break;
+    case 18:
+      document.write("La letra correspondiente es V");
+      break;
+    case 19:
+      document.write("La letra correspondiente es H");
+      break;
+    case 20:
+      document.write("La letra correspondiente es L");
+      break;
+    case 21:
+      document.write("La letra correspondiente es C");
+      break;
+    case 22:
+      document.write("La letra correspondiente es K");
+      break;
+    case 23:
+      document.write("La letra correspondiente es E");
+      break;
+  }
+  alert("Número de DNI válido.");
 } else {
-    alert('El número de DNI debe estar entre 0 y 99999999.');
+  alert("El número de DNI debe estar entre 0 y 99999999.");
 }
