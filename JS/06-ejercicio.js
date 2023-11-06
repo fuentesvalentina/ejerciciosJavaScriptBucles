03a8829e2df25e9d5992f220c18d26bacc9f750f
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-11-05 21:37:47 -0300

    Agrego ejercicio numero 6 completo

---
+++ JS/06-ejercicio.js
@@ -0,0 +1,22 @@
+/*Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+…….
+*/
+
+document.write("<pre>");
+for (let i = 1; i <= 30; i++) {
+  let linea = '';
+  for (let j = 1; j <= i; j++) {
+    linea = linea + i;
+  }
+  document.write(linea + "<br>");
+}
+document.write("</pre>");
+
+  
