a71537b033a8a365aff1c952d7a230bb37226ca7
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-11-06 10:45:21 -0300

    Agrego ejercicio numero 4

---
+++ JS/04-ejercicio.js
@@ -0,0 +1,29 @@
+/* Realiza un script que pida números hasta que se pulse
+ “cancelar”. Si no es un número deberá indicarse con un
+  «alert» y seguir pidiendo números. Al salir con “cancelar” 
+  deberá indicarse la suma total de los números introducidos.*/
+
+let suma = 0;
+let continuar = true;
+
+while (continuar) {
+  let entrada = prompt("Ingrese un número:");
+
+  if (entrada === null) {
+    continuar = false; // El usuario pulsó "cancelar"
+  } else {
+    let numero = parseFloat(entrada);
+
+    if (!isNaN(numero)) {
+      suma += numero;
+    } else {
+      alert("Por favor, ingrese un número válido.");
+    }
+  }
+}
+
+if (!isNaN(suma)) {
+  alert("La suma total de los números introducidos es: " + suma);
+} else {
+  alert("No se ingresaron números.");
+}
